eece91e5af0be3bb67d84809916cf6867e780739
Jacky Zhao <yzhao8056@gmail.com>
2025-06-17 20:08:07 -0400

    Added new css and js files for styling and scrolling controls

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const nav = document.getElementById('navbar');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 10) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+});
