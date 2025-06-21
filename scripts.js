0a4611b6c2b0ad6f3572cf769958bb731f9d1e05
Yun Zhao <icecream@mit.edu>
2025-06-21 18:44:01 -0400

    Changed the formatting of the file structures. Changed the background to white.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const nav = document.getElementById('navbar');
 
+// Implements the mouse scrolling functionality.
 window.addEventListener('scroll', () => {
     if (window.scrollY > 10) {
         nav.classList.add('scrolled');
