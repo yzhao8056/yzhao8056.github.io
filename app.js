924ac6d6597fa16f9262c7cee94bbad1dcc7ab59
Yun Zhao <icecream@mit.edu>
2025-06-27 02:37:24 -0400

    Added the project section with fade-in effects.

--- app.js
+++ app.js
@@ -8,3 +8,19 @@ window.addEventListener('scroll', () => {
         nav.classList.remove('scrolled');
     }
 });
+
+// Implements the fade-in behavior.
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+            observer.unobserve(entry.target);
+        }
+    });
+}, {
+    threshold: 0.5
+});
+
+document.querySelectorAll('.fade-in').forEach(ele => {
+    observer.observe(ele);
+});
